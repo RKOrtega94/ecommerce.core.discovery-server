31e856e2fa470a54e940de298d802fe7b15934c2
RKOrtega94 <44837907+RKOrtega94@users.noreply.github.com>
2025-10-01 22:28:02 -0500

    Initialize discovery server with Spring Boot setup and configuration

---
+++ src/main/java/ec/com/ecommerce/DiscoveryServerApplication.java
@@ -0,0 +1,15 @@
+package ec.com.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class DiscoveryServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServerApplication.class, args);
+    }
+}
